65e58e8baee5481137245889635f49c95ebc3f0f
bella <catgirlsorsmth@proton.me>
2025-10-09 17:58:04 -0700

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Loader
+// @namespace    http://tampermonkey.net/
+// @version      2025-10-09
+// @author       Bella
+// @match        https://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/ravegirls/BetterECS/refs/heads/main/source.js
+// @downloadURL  https://raw.githubusercontent.com/ravegirls/BetterECS/refs/heads/main/source.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+})();
