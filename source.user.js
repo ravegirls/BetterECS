5ff7a14a194eb7574f2a4552004ab62a1167dfab
bella <catgirlsorsmth@proton.me>
2025-10-09 18:25:34 -0700

    publish new build [automatic]

--- source.user.js
+++ source.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BetterECS
 // @namespace    http://tampermonkey.net/
-// @version      0.0.10
+// @version      0.0.11
 // @description  Economy Simulator Overhaul Plugin (Dev)
 // @author       isabella
 // @match        https://ecsr.io/*
